317f753be0ecdc89b7b1b9d7c311f48efc2b7e6b
Danilo Marto de Carvalho <euDaniloMarto@gmail.com>
2025-08-22 02:14:32 -0300

    add admin.py module to cliente app

--- CRM/clientes/admin.py
+++ CRM/clientes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Cliente
 
-# Register your models here.
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "nome",
+        "tipo_cliente",
+    )
+    prepopulated_fields = {"slug": ("nome",)}
+    search_fields = ("nome",)
+    list_filter = ("tipo_cliente", "ativo", "alarme", "camera")
+    ordering = ("nome", "ativo")
+    date_hierarchy = "criado_em"
